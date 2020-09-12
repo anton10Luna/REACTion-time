cb8076550202bd873fff012998af75412444f157
anton10Luna <antonio.gilluna@gmail.com>
2020-09-11 22:16:12 -0700

    fist commit, eddited files to keep files needed, eddited webpack, package-json, readme, serve, clinet, db

---
+++ README.md
@@ -0,0 +1,7 @@
+# REACTion Time #
+
+## Heading ##
+  > Test your reaction time and find out go fast you can get with the mouse and click.
+
+## How to Get Started ##
+  > load the application, click on the ready button, move over to the setting area and wait for the signal to move over and click.
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
-var path = require('path');
-var SRC_DIR = path.join(__dirname, '/react-client/src');
-var DIST_DIR = path.join(__dirname, '/react-client/dist');
+const path = require('path');
+const SRC_DIR = path.join(__dirname, '/client/src');
+const DIST_DIR = path.join(__dirname, '/client/dist');
 
 module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
@@ -8,15 +8,30 @@ module.exports = {
     filename: 'bundle.js',
     path: DIST_DIR
   },
-  module : {
-    loaders : [
+  module: {
+    loaders: [
       {
-        test : /\.jsx?/,
-        include : SRC_DIR,
-        loader : 'babel-loader',      
+        test: /\.jsx?/,
+        include: SRC_DIR,
+        loader: 'babel-loader',
         query: {
           presets: ['react', 'es2015']
-       }
+        }
+      },
+      {
+        test: /\.scss$/i,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            }
+          },
+          {
+            loader: 'sass-loader',
+          }
+        ],
       }
     ]
   }
